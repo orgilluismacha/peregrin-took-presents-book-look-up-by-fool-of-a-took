e1fd168428ba52664215870488ac6b6868d99d52
orgilluismacha <orgilluismacha@gmail.com>
2022-05-10 12:28:44 +0200

    Setting up new components

---
+++ src/app/Book.ts
@@ -0,0 +1,7 @@
+export interface Book {
+  id?: number;
+  title: string;
+  author: string;
+  details: string;
+  coverURL : string;
+}
---
+++ src/app/mock-boox.ts
@@ -0,0 +1,26 @@
+import {Book} from './Book'
+
+export const BOOKS: Book[]= [
+  {
+    id: 0 ,
+    title: "LOTR: 1",
+    author: "JRR TOLKIEN",
+    details: "Hobbit finds out his uncle's dark secrets",
+    coverURL: "https://images-na.ssl-images-amazon.com/images/I/41gHG-a2OEL._SX331_BO1,204,203,200_.jpg"
+  },
+  {
+    id: 2 ,
+    title: "LOTR: 2",
+    author: "jay AR AR TOKEN",
+    details: "Shit hits the fan for the aforementioned hobbit",
+    coverURL: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/The_Two_Towers_cover.gif/220px-The_Two_Towers_cover.gif"
+  },
+  {
+    id: 3 ,
+    title: "LOTR: 3",
+    author: "jay ARGH ARGH TOKEN",
+    details: "Shit finally settles down",
+    coverURL: "https://image.bokus.com/images2/9780261103573_200x_the-fellowship-of-the-ring"
+  },
+  
+]
